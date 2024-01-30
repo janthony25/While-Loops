5496c3198f7848f39e8bb60a9a8c26bb15b27115
janthony25 <junanthony.business@gmail.com>
2024-01-30 23:15:28 +1300

    Add project files.

---
+++ While-Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jun Anthony";
+            string password = "0123456a";
+
+;            string pass = "";
+
+            while (pass != password)
+            {
+                Console.Write("Please enter the right password: ");
+                pass = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {name}");
+
+            Console.ReadKey();
+        }
+    }
+}
